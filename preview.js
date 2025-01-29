c3eb732fdeae231d6b7cb14ff0f977abc30001b4
LucasPulgaci032 <163359576+LucasPulgaci032@users.noreply.github.com>
2025-01-29 14:04:56 -0300

    Add files via upload

---
+++ preview.js
@@ -0,0 +1,23 @@
+/** @type { import('@storybook/react').Preview } */
+import { Provider } from 'react-redux';
+import store from '../src/store'; 
+
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <Provider store={store}>
+        <Story />
+      </Provider>
+    ),
+  ],
+};
+
+export default preview;
